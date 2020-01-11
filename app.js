2350c375c30a75871aa16060f145c9b96d980417
Alex2Pena <attn.Alex.P@gmail.com>
2020-01-10 17:40:34 -0800

    completed problem4

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ return [sumOfThreeNumbers, yz, a + ' and ' + b + ' and ' + c + ' sum to ' + sumO
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-testSumAndMultiply(4,7,5);
+// testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -91,12 +91,18 @@ Test this function by hand in the console to get it working, and when you think 
 var testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
-
+var a = testArray[0];
+var b = testArray[1];
+var c =  testArray[2];
+var sumOfTwo = sum(a,b)[0];
+var sumOfThree = sum(sumOfTwo,c)[0];
+// console.log(sumOfThree);
+return [sumOfThree, + a + ',' + b + ',' + c +  ' was passed in as an array of numbers, and ' + sumOfThree + ' is their sum.']
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray); 
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
